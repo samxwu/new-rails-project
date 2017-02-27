d60badcf453a2ac0d565afe94bb3a8f501fcc0b1
Sam Wu <sam.x.wu@gmail.com>
2017-02-27 19:31:50 +0000

    Checkpoint 6 Seeding Data Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,41 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# Create Wikis
+# Create Public Wikis
 
-15.times do Wiki.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph)
+10.times do Wiki.create!(
+    title: Faker::Book.unique.title, 
+    body: RandomData.random_paragraph
+    )
 end
 
-15.times do Wiki.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, private: true)
+# Create Private Wikis
+10.times do Wiki.create!(
+    title: Faker::Book.unique.title, 
+    body: RandomData.random_paragraph, 
+    private: true
+    )
 end
 
 wikis = Wiki.all
 
+
+#Create Users
+
+15.times do 
+    
+    user = User.new(
+        email: Faker::Internet.unique.email, 
+        role: [0,1,2].sample, 
+        password: 'password', 
+        password_confirmation: 'password',
+        confirmed_at: Faker::Time.between(DateTime.now - 1, DateTime.now)
+    )
+    user.skip_confirmation!
+    user.save!
+end
+users = User.all
+
 puts "Seed finished"
 puts "#{Wiki.count} wikis created"
+puts "#{User.count} users created"
