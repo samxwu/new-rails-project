f40d261931c8366d32ce4984aa364f2194063aaa
Sam Wu <sam.x.wu@gmail.com>
2017-03-01 21:44:03 +0000

    Checkpoint 9 Markdown Completed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
+require 'redcarpet'
 module ApplicationHelper
+    def markdown(text)
+        options = {
+            hard_wrap:              true, 
+            filter_html:            true, 
+            autolink:               true, 
+            no_intra_emphasis:      true, 
+            fenced_code_blocks:     true,
+            space_after_headers:    true
+        }
+        
+        extensions = {
+            autolink:           true,
+            superscript:        true,
+            disable_indented_code_blocks: true
+        }
+    
+        renderer = Redcarpet::Render::HTML.new(options)
+        markdown = Redcarpet::Markdown.new(renderer, extensions)
+        
+        markdown.render(text).html_safe
+    end
 end
